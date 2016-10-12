68d37ba5387bdbb29c3d99d65ae0078db6bd257c
Won Yong Ha <Owl@140-182-199-16.dhcp-bl.indiana.edu>
2016-10-11 21:47:48 -0400

    11 October 2016

---
+++ crypt.py
@@ -0,0 +1,21 @@
+#---------------------------------------------------
+#
+# Won Yong Ha
+#
+# Python
+#
+# Cryptography Project
+#
+#---------------------------------------------------
+
+#Variable
+
+
+#main Function
+def main():
+
+    #Getting Name
+    textName = input("Enter the document name(.txt): ")
+    outputName = input("Enter the name of the result file: ")
+    
+    
